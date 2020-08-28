bfebd8086b03a15bf43989f9a7dc659f0de3ae4e
Ian DeVivi <Ian.DeVivi@barings.com>
2020-08-28 12:15:06 -0400

    controller add

---
+++ StarChart/Controllers/CelestialObjectsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        public CelestialObjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
